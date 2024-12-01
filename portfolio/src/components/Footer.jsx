c5a52ce67716aeb0bce12349e5874d8c0cb85136
patelSahil9 <ptulasi578@gmail.com>
2024-12-01 19:07:56 +0530

    .

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p>© 2023 Andy Hardy</p>
+    </div>
+  );
+};
+
+export default Footer;
